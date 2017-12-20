4b1d197139d3fc1e388fe0448c3e17b38156f503
Lilly Ryan <lilly@attacus.net>
2017-12-20 16:40:19 +1100

    updating test

--- tasks.py
+++ tasks.py
@@ -19,11 +19,11 @@ def rubbish_reminder(ctx, recycling=False):
         client.messages.create(to=os.environ['HOUSE_PHONES'], from_=os.environ['TWILIO_PHONE'], body=message)
 
 @task
-def test(ctx, recycling=False):
+def test(ctx, unicors=False):
     client = TwilioRestClient(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-    if recycling:
-        message = "TEST Recycling and general rubbish needs to go out tonight."
+    if unicorns:
+        message = "OMG unicorns!"
         client.messages.create(to=os.environ['LILLY_PHONE'], from_=os.environ['TWILIO_PHONE'], body=message)
     else:
-        message = "TEST The rubbish bin needs to go out tonight. No recycling this week."
+        message = "The unicorn escaped."
         client.messages.create(to=os.environ['LILLY_PHONE'], from_=os.environ['TWILIO_PHONE'], body=message)
