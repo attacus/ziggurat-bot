386e4274b23d0ded458bb5883417dd29c1f7cd9f
Lilly Ryan <lilly@attacus.net>
2017-12-21 11:11:23 +1100

    Upgrading Twilio REST Client version

--- tasks.py
+++ tasks.py
@@ -1,34 +1,34 @@
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 from invoke import task
 import os
 
 
 @task
 def lunch_reminder(ctx):
-    client = TwilioRestClient(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-    client.messages.create(to=os.environ['MAX_PHONE'], from_="ZigguratBot", body="Bring your lunch to work. It's in the fridge.")
+    client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+    client.messages.create(to=os.environ['MAX_PHONE'], from_="ZigguratBot", body="🍗 Bring your lunch to work. It's in the fridge.")
 
 @task
 def compliment(ctx):
-    client = TwilioRestClient(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-    client.messages.create(to=os.environ['MAX_PHONE'], from_="ZigguratBot", body="Beep boop. I hope you have a nice day today.")
+    client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+    client.messages.create(to=os.environ['MAX_PHONE'], from_="ZigguratBot", body="Beep boop. Also you are nice. ✨")
 
 @task
 def rubbish_reminder(ctx, recycling=False):
-    client = TwilioRestClient(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+    client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
     if recycling:
-        message = "Recycling and general rubbish needs to go out tonight."
+        message = "♻️! Recycling and general rubbish needs to go out tonight."
         client.messages.create(to=os.environ['HOUSE_PHONES'], from_="ZigguratBot", body=message)
     else:
-        message = "The rubbish bin needs to go out tonight. No recycling this week."
+        message = "🗑️🗑️🗑️ The rubbish bin needs to go out tonight. No recycling this week."
         client.messages.create(to=os.environ['HOUSE_PHONES'], from_="ZigguratBot", body=message)
 
 @task
 def test(ctx, unicorns=False):
-    client = TwilioRestClient(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+    client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
     if unicorns:
-        message = "OMG unicorns!🦄🦄🦄"
+        message = "OMG unicorns! 🦄🦄🦄"
         client.messages.create(to=os.environ['LILLY_PHONE'], from_="ZigguratBot", body=message)
     else:
-        message = "The unicorn escaped.🚫🦄"
+        message = "The unicorn escaped. 🚫🦄"
         client.messages.create(to=os.environ['LILLY_PHONE'], from_="ZigguratBot", body=message)
